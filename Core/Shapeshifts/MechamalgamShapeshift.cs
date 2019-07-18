37b00dedbf02987b7418120aac687c82eeb5a2ba
Aurian <theorian34@gmail.com>
2019-07-18 19:32:33 +0200

    typo

--- Core/Shapeshifts/MechamalgamShapeshift.cs
+++ Core/Shapeshifts/MechamalgamShapeshift.cs
@@ -88,7 +88,7 @@ namespace Shapeshifter.Core.Shapeshifts
 				{
 					player.statLife = 0;
 					Main.PlaySound(4 , player.position, 14);
-					player.KillMe(PlayerDeathReason.ByCustomReason("Hydrolic Surcharge! System Shut Down."), 10.0, 0, false);
+					player.KillMe(PlayerDeathReason.ByCustomReason("Hydraulic Surcharge! System Shut Down."), 10.0, 0, false);
 				}
 				if (Main.rand.Next(120) == 0)
 				{
